748a2e8d3d6bf091b87d3a4096f5590d2b696429
Adenle Abiodun <adenleabbey@gmail.com>
2024-04-24 13:44:58 +0100

    chore: update Vercel Speed Insghts and Analytics 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+// import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Component {...pageProps} />
       <Analytics />
-      <SpeedInsights />
+      {/* <SpeedInsights /> */}
     </>
   );
 }
