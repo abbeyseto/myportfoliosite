8ea2cdc5c728cedf7fe40a1cfc90a306e9f396f2
Adenle Abiodun <adenleabbey@gmail.com>
2024-09-30 10:59:05 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   pages/_app.tsx 	new file:   public/.well-known/assetlinks.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { Analytics } from "@vercel/analytics/react";
-// import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -8,7 +7,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Component {...pageProps} />
       <Analytics />
-      {/* <SpeedInsights /> */}
     </>
   );
 }
